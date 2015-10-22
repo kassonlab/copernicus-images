0c807a09c66bb24cd1d566c2a498c5c3264675e8
Peter Kasson <tg457513@login3.stampede.tacc.utexas.edu>
2015-10-22 03:53:22 -0500

    Added pythonpath and msmbuilder extraction

--- stampede/cpc_setup.sh
+++ stampede/cpc_setup.sh
@@ -3,9 +3,10 @@
 export CPC_DATA=$WORK/cpc-data
 export CPC_HOME=$HOME/copernicus
 export PATH=$PATH:$CPC_HOME
+export PYTHONPATH=$PYTHONPATH:$HOME
 module load hdf5
 module load python
 module load gromacs
 
 cpc-server setup $CPC_DATA
-
+tar zxvf $CPC_HOME/examples/msm/msmbuilder-known-good.tar.gz
