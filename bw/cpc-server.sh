7c430a1f11c10b44844f61b61c934c1a6d09633c
Kassonlab <kassonlab>
2016-08-06 12:47:48 -0500

    run on MOM nodes

--- bw/cpc-server.sh
+++ bw/cpc-server.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 #PBS -N cpc_server
-#PBS -o "cpc_server.%j.%N.out"
-#PBS -e "cpc_server.%j.%N.err"
 #PBS -l nodes=1:ppn=16:xe
 #PBS -V
 #PBS -l walltime=02:00:00
@@ -10,11 +8,14 @@ module load bwpy
 # module load gromacs
 # kludge to use static pre-build gromacs binaries
 export PATH=$PATH:/u/training/instr015/bin
+export GMXLIB=/u/training/instr015/share/top
 
 export CPC_DATA=$HOME/cpc-data
 export CPC_HOME=$HOME/copernicus
 export PATH=$PATH:$CPC_HOME
-aprun cpc-server start
-cpc-server bundle -o local_bundle.cnx
+export PYTHONPATH=$PYTHONPATH:$HOME:$CPC_HOME
+aprun hostname
+cpc-server start
+cpc-server bundle -o local_bundle2.cnx
 sleep 172800
 # 48 hours
